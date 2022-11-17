a019ca461c582d8d48035248e28ea1bf3ff2f715
patrice_dev <patricedev02@gmail.com>
2022-11-17 15:23:25 +0000

    update the product views by using model_to_dict

--- cart/models.py
+++ cart/models.py
@@ -2,24 +2,24 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
-
 from product.models import Product
 
 from .mixins import DateMixin
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
 
 STATE = (
-    ('pending','Pending'),
-    ('success','Success'),
-   ( 'cancel','Cancel'),
+    ('pending', 'Pending'),
+    ('success', 'Success'),
+    ('cancel', 'Cancel'),
 )
 
 
 class Cart(DateMixin):
     cart_user = models.OneToOneField(User, verbose_name=_("Cart user"), on_delete=models.CASCADE)
-    cart_state = models.CharField(_("Cart state"),choices = STATE, max_length=50, default="Pending")
+    cart_state = models.CharField(_("Cart state"), choices=STATE, max_length=50, default="Pending")
     transaction_id = models.CharField(_("Transaction id"), max_length=100)
     complete = models.BooleanField(_("Complete cart"), default=False)
 
@@ -28,24 +28,19 @@ class Cart(DateMixin):
 
     @property
     def cart_total_cost(self):
-        s  = sum([cart_item.cart_item_total_cost  for cart_item in self.cartitem_set.all()]) 
+        s = sum([cart_item.cart_item_total_cost for cart_item in self.cartitem_set.all()])
         return s
-    
+
 
 class CartItem(DateMixin):
     product = models.ForeignKey(Product, verbose_name=_("Cart product"), on_delete=models.CASCADE)
     cart_id = models.ForeignKey(Cart, verbose_name=_("Relate cart"), on_delete=models.CASCADE)
-    product_quantity = models.PositiveIntegerField(_("Cart item product"), default = 0)
-    cart_item_state = models.CharField(_("Product state"), choices= STATE, max_length=50)
+    product_quantity = models.PositiveIntegerField(_("Cart item product"), default=0)
+    cart_item_state = models.CharField(_("Product state"), choices=STATE, max_length=50)
 
     def __str__(self):
         return str(self.product)
 
-
     @property
     def cart_item_total_cost(self):
         return int(self.product.product_finaly_price) * int(self.product_quantity)
-    
-
-
-
